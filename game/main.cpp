e25a1a45c4a5bb643563e6bdd5c8181483798651
p1rattttt <alekseevmaksim63@list.ru>
2021-03-21 17:37:32 +0300

    UPD

--- game/main.cpp
+++ game/main.cpp
@@ -1,9 +1,9 @@
-#include <bits/stdc++.h>
 #include "objects/object.h"
 #include "objects/objectController.h"
 #include "objects/ceil.h"
 #include "objects/strategy.h"
 #include "objects/field.h"
+#pragma "once"
 
 int main() {
 
