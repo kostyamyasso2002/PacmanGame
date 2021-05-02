413c97e814e5988b72cd6e07bc7eb2b08c91f14f
Konstantin Myasnikov <miasnikov.km@phystech.edu>
2021-05-02 21:07:11 +0300

    changed names

--- game/main.cpp
+++ game/main.cpp
@@ -11,12 +11,6 @@ int main() {
   PacManControllerCreator a;
   std::shared_ptr<PacManController> t = std::static_pointer_cast<PacManController>(a.CreateObjectController());
   t->Move(1, 2);
-  std::shared_ptr<SimpleDrawer> b = std::make_shared<BasicSimpleDrawer>();
-  std::shared_ptr<SimpleDrawer> kek = std::make_shared<SimpleDrawerDecoratorA>(b);
-  std::shared_ptr<SimpleDrawer> lol = std::make_shared<SimpleDrawerDecoratorA>(kek);
-
-  lol->AddString("lol");
-  lol->out();
 
 
   return 0;
